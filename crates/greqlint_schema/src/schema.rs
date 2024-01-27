419d2fdab2adf110ffc056ad89a431f84c26e557
Ubugeeei <ubuge1122@gmail.com>
2024-01-27 13:33:13 +0900

    update schema

--- crates/greqlint_schema/src/schema.rs
+++ crates/greqlint_schema/src/schema.rs
@@ -4,9 +4,16 @@ use serde::{Deserialize, Serialize};
 #[derive(Deserialize, Serialize, JsonSchema, Debug)]
 pub struct Schema {
     pub provider: Provider,
+
     pub types: Vec<CommitType>,
+
     pub common_scopes: Option<Vec<String>>,
+
+    #[schemars(description = "available variables: ${type} ${scope} ${mr} ${issues}")]
+    #[serde(rename = "titleFormat")]
     pub title_format: Option<String>,
+
+    #[serde(rename = "branchNameFormat")]
     pub branch_name_format: Option<String>,
 }
 
@@ -14,8 +21,10 @@ pub struct Schema {
 pub enum Provider {
     #[serde(rename = "github-actions")]
     GitHubActions,
+
     #[serde(rename = "gitlab-runner")]
     GitLabRunner,
+
     #[serde(rename = "type")]
     Custom,
 }
@@ -24,5 +33,6 @@ pub enum Provider {
 pub struct CommitType {
     #[serde(rename = "type")]
     pub type_name: String,
+
     pub scopes: Option<Vec<String>>,
 }
