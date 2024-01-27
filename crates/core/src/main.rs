fec55d502b225db664a0d51aa87f609752d0c066
Ubugeeei <ubuge1122@gmail.com>
2024-01-27 12:23:46 +0900

    feat: config loader

--- crates/core/src/main.rs
+++ crates/core/src/main.rs
@@ -1,3 +1,13 @@
+use std::path;
+
+use loader::load;
+
+mod loader;
+
 fn main() {
-    println!("Hello, world!");
+    // TODO: path
+    let config_path = path::Path::new("./schema/example/.greqlint");
+
+    let schema = load(config_path.to_str().unwrap());
+    println!("{:?}", schema);
 }
