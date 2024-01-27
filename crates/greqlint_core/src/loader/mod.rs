ec9b6af1775200fbcd0e07140ed97d98b058117a
Ubugeeei <ubuge1122@gmail.com>
2024-01-27 12:40:35 +0900

    feat: schema generator

--- crates/greqlint_core/src/loader/mod.rs
+++ crates/greqlint_core/src/loader/mod.rs
@@ -1,8 +1,6 @@
 use schemars::schema::Schema;
 use serde_json::from_str;
 
-pub mod schema;
-
 pub fn load(path: &str) -> Result<Schema, Box<dyn std::error::Error>> {
     let res = from_str::<Schema>(&std::fs::read_to_string(path)?)?;
     Ok(res)
